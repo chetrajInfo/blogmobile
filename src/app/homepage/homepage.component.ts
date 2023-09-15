4110b6527612f65051958088d4865355c1f1b1cc
myinfo <chetraj0101@gmail.com>
2023-09-15 15:44:38 -0500

    added footer and section part for the appleInc

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  data: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
